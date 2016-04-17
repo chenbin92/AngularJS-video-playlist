fcd3481d4844720999ab39cb341c5b88710b4d33
Bruce Chen <bruce@beansmile.com>
2016-04-17 22:51:50 +0800

    Clear unnecessary code

--- www/js/app.js
+++ www/js/app.js
@@ -59,15 +59,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
 
   .state('tab.account', {
     url: '/account',
