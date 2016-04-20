48a648b536e6dd2425bcc7c3a2cfa8225dcefc96
Bruce Chen <bruce@beansmile.com>
2016-04-20 11:20:47 +0800

    Update the video resources

--- www/js/app.js
+++ www/js/app.js
@@ -82,6 +82,6 @@ angular.module('starter', [
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/example1');
 
 });
